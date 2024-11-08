4db347822b34ed5ff8810e67d415e6a94248e12f
Jenadius Nicholaus <ucode888@gmail.com>
2024-11-08 16:18:20 +0300

    updated the sql connector

--- settings/settings.py
+++ settings/settings.py
@@ -13,11 +13,14 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from decouple import config
 from datetime import timedelta
+import pymysql
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+pymysql.install_as_MySQLdb()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
