1db8bc61d24c979cdea22e86f83831d4afc7cd5e
Jenadius Nicholaus <ucode888@gmail.com>
2024-11-08 16:52:29 +0300

    add the static code here

--- settings/settings.py
+++ settings/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from decouple import config
 from datetime import timedelta
 import pymysql
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -137,6 +138,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
